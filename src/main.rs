fc21471e3d7ff686746aa4cb0200f19b03c436f9
Joseph Kato <joseph@jdkato.io>
2023-05-23 16:58:00 -0700

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,46 @@
-use clap::Parser;
+use std::path;
+
+use clap::{Parser, Subcommand};
+
+use libvale::vale::ValeManager;
 
 /// A helper utility for running Vale in CI environments.
 #[derive(Parser, Debug)]
 #[command(version)]
-struct Args;
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Installs a specific version of Vale to the specified path.
+    Install {
+        path: path::PathBuf,
+        version: Option<String>,
+    },
+}
 
 fn main() {
-    let _ = Args::parse();
-    println!("Hello, world!");
+    let cli = Args::parse();
+    match &cli.command {
+        Some(Commands::Install { path, version }) => {
+            let manager = ValeManager::new();
+
+            let mut version = match version {
+                Some(v) => v,
+                None => "latest",
+            };
+
+            if version.starts_with("v") {
+                version = version.trim_start_matches("v");
+            }
+
+            match manager.install(&path, version) {
+                Ok(_) => println!("Installed {:?} to {:?}", version, path),
+                Err(e) => println!("Error: {:?}", e.to_string()),
+            }
+        }
+        _ => {}
+    }
 }
--- src/vale.rs
+++ src/vale.rs
@@ -2,12 +2,20 @@ use std::{io, path};
 
 use flate2::read::GzDecoder;
 use reqwest;
+use serde::Deserialize;
 use tar::Archive;
 
 use crate::error::Error;
 use crate::utils::vale_arch;
 
+// https://github.com/errata-ai/vale/releases/download/v2.27.0/vale_2.27.0_macOS_arm64.tar.gz
 const RELEASES: &str = "https://github.com/errata-ai/vale/releases/download";
+const LATEST: &str = "https://api.github.com/repos/errata-ai/vale/releases/latest";
+
+#[derive(Deserialize, Debug)]
+pub(crate) struct Release {
+    tag_name: String,
+}
 
 #[derive(Debug, Clone)]
 pub struct ValeManager {
@@ -36,10 +44,15 @@ impl ValeManager {
     /// * `path` - A path to the directory where Vale should be installed.
     /// * `version` - A string representing the version to be installed.
     /// * `arch` - A string representing the architecture to be installed.
-    pub fn install(&self, path: &path::Path, v: &str, arch: &str) -> Result<(), Error> {
-        let mut asset = format!("/v{}/vale_{}_{}.tar.gz", v, v, arch);
-        if arch.to_lowercase().contains("windows") {
-            asset = format!("/v{}/vale_{}_{}.zip", v, v, arch);
+    pub fn install(&self, path: &path::Path, version: &str) -> Result<(), Error> {
+        let mut v = version.to_string();
+        if v == "latest" {
+            v = self.fetch_version()?;
+        }
+
+        let mut asset = format!("/v{}/vale_{}_{}.tar.gz", v, v, self.arch);
+        if self.arch.to_lowercase().contains("windows") {
+            asset = format!("/v{}/vale_{}_{}.zip", v, v, self.arch);
         }
         let url = format!("{}{}", RELEASES, asset);
 
@@ -54,5 +67,18 @@ impl ValeManager {
         }
 
         Ok(())
+    }
+
+    /// `fetch_version` returns the latest version of Vale.
+    fn fetch_version(&self) -> Result<String, Error> {
+        let client = reqwest::blocking::Client::builder()
+            .user_agent("libvale")
+            .build()?;
+
+        let resp = client.get(LATEST).send()?;
+        let info: Release = resp.json()?;
+
+        let tag = info.tag_name.strip_prefix("v").unwrap().to_string();
+        Ok(tag)
     }
 }
